4309b04ad5320e6629bae95b7c6e1d5d37f75e21
Kamboj <2401618@cognizant.com>
2025-05-16 17:40:34 +0530

    added project

---
+++ socialmediaapp/src/test/java/com/techtians/socialmediaapp/SocialMediaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techtians.socialmediaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
